b9f90084cc73ce54b6c810f4ef66de54014e8d62
ursueugen <ursu_eugen@hotmail.com>
2022-11-15 13:51:51 +0200

    Planned

--- Readme.md
+++ Readme.md
@@ -12,3 +12,16 @@ Follow steps from the [Python Quickstart for Google Drive API](https://developer
 pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
 ```
 - Create a sample `quickstart.py` and explore.
+
+Documentation:
+- [Google Drive API Python](https://developers.google.com/resources/api-libraries/documentation/drive/v3/python/latest/index%2Ehtml)
+- [Google Drive API REST Reference](https://developers.google.com/drive/api/v3/reference)
+- Many useful guides, especially on `Manage files & folders`
+    - [Search for files & folders](https://developers.google.com/drive/api/guides/search-files#python)
+    - [Create and populate folders](https://developers.google.com/drive/api/guides/folder)
+
+## Plan for migration
+- [X] Create a new folder `Migration` in the destination team drive.
+- [] Parse directory structure in the source team drive.
+- [] Create the same directory structure in the destination team drive.
+- [] Copy files from source to destination.
--- quickstart.py
+++ quickstart.py
@@ -1,41 +1,21 @@
 from __future__ import print_function
-
-import os.path
-
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from utils import get_creds, get_gdrive_api_service
 
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+# For more info on scopes, check https://developers.google.com/identity/protocols/oauth2/scopes#drive
+SCOPES = ["https://www.googleapis.com/auth/drive"]  # See, edit, create, and delete all of your Google Drive files
+
 
 def main():
     """Shows basic usage of the Drive v3 API.
     Prints the names and ids of the first 10 files the user has access to.
     """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
+    creds = get_creds()
     
     try:
-        service = build('drive', 'v3', credentials=creds)
+        service = get_gdrive_api_service(creds)
         # Call the Drive v3 API
         results = service.files().list(
             pageSize=10, fields="nextPageToken, files(id, name)").execute()
